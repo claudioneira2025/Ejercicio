8041aa85f32b72ed6863c2e0072dd36b589be68f
claudioneira2025 <claudio.neira15@inacapmail.cl>
2025-11-05 00:05:37 -0300

    main veterinaria

--- TiendaMascotas/macotas.py
+++ TiendaMascotas/macotas.py
@@ -1,9 +1,9 @@
 class Mascota:
 
-def __init__(self,nombre,especie,edad):
-    self.nombre=nombre
-    self.especie=especie
-    self.edad=edad
+    def __init__(self,nombre,especie,edad):
+        self.nombre=nombre
+        self.especie=especie
+        self.edad=edad
 
 def mostrar_datos(self):
         return f"nombre {self.nombre} -especie {self.especie}- edad{self.edad}"
@@ -12,8 +12,13 @@ print(Mascota.mostrar_datos())
 
 lista_mascotas=["perro","gato","otros"]
 
-nueva_mascota=append(nueva_mascota)
+nueva_mascota=append["nueva_mascota"]
+
+
+
+print("Tu lista de mascotas es:",lista_mascotas )
 
 nombre=input(self.nombre)
 
-print("Tu lista de mascotas es:",lista_mascotas )
+if nombre == "nombre":
+     
---
+++ TiendaMascotas/veterinaria.py
@@ -0,0 +1,10 @@
+from mascota import Mascota
+
+class veterinaria:
+    def __init__(self,nombreVeterinaria):
+        self.nombreVeterinaria=nombreVeterinaria
+        self.mascotas=[]
+
+def registrar_mascota(self,mascota:Mascota):
+    
+    

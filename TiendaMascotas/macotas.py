339c61c372d809cbbf92a9c72804529c70715d56
claudioneira2025 <claudio.neira15@inacapmail.cl>
2025-11-04 21:23:01 -0300

    Nuevacarpetamascotas

---
+++ TiendaMascotas/macotas.py
@@ -0,0 +1,19 @@
+class Mascota:
+
+def __init__(self,nombre,especie,edad):
+    self.nombre=nombre
+    self.especie=especie
+    self.edad=edad
+
+def mostrar_datos(self):
+        return f"nombre {self.nombre} -especie {self.especie}- edad{self.edad}"
+
+print(Mascota.mostrar_datos())
+
+lista_mascotas=["perro","gato","otros"]
+
+nueva_mascota=append(nueva_mascota)
+
+nombre=input(self.nombre)
+
+print("Tu lista de mascotas es:",lista_mascotas )
